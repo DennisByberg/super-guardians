b07ddfcd18c177fa69ae4577bcc15c28ccdbec65
Duserdumut <danielmalmborg@hotmail.com>
2023-03-28 00:55:47 +0200

    Almost done

--- super-guardians/src/Pages/Status/Status.jsx
+++ super-guardians/src/Pages/Status/Status.jsx
@@ -1,7 +1,22 @@
 import "./Status.css";
+import { Link } from "react-router-dom";
 
 function Status() {
-  return <section>Status</section>;
+  return <section className="status">  
+  <div className="order_Number_Container">
+  <p className="status_Number_Text">Ordernumber </p>
+  <p className="status_Number">#12DV23F</p>
+  </div>
+ 
+  <img className="drone_pic" src="src/assets/DRONE.svg"/>
+       <h1 className="status_Timer_Text">Din beställning är på väg!</h1>
+       <h2 className="status_Timer">13 minuter</h2>
+       <Link className="button_Status" to="/">
+        Ok, Cool!
+      </Link>
+      
+  </section>;
 }
 
 export default Status;
+/* status_button */
